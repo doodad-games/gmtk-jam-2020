27784d3ab771d92a3cbc1313b8eb6872b75e31c6
Bilal Akil <mail@bilalakil.me>
2020-07-12 18:00:46 +1000

    Pan loop, added music

---
+++ Assets/Other/Scripts/PanLoop.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/*
+Object to loop should have a plain transform (0 local position/rotation, 1 scale).
+Make modifications to a parent transform instead of changing this.
+
+Script will create a copy of the game object and move both along the X axis only -
+which is why rotating this game object will ruin the illusion.
+ */
+public class PanLoop : MonoBehaviour
+{
+#pragma warning disable CS0649
+    [SerializeField] float _xPerSec;
+    [SerializeField] float _xPeriod;
+#pragma warning restore CS0649
+
+    GameObject _other;
+
+    void Start()
+    {
+        _other = Instantiate(
+            gameObject,
+            transform.parent
+        );
+        _other.transform.localPosition = new Vector3(-_xPeriod, 0f, 0f);
+        _other.transform.rotation = transform.rotation;
+
+        Destroy(_other.GetComponent<PanLoop>());
+    }
+
+    void Update()
+    {
+        var curX = transform.localPosition.x;
+        var newX = (curX + _xPerSec * Time.deltaTime) % _xPeriod;
+
+        transform.localPosition = new Vector3(newX, 0f, 0f);
+        _other.transform.localPosition = new Vector3(-_xPeriod + newX, 0f, 0f);
+    }
+}
